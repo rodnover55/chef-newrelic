5c0643e1becb85baf1878a020f53f0370f4d5ea8
Melnikov Sergey <me@rnr.name>
2014-07-25 14:21:22 +0700

    Fixed setting name.

--- recipes/server-agent.rb
+++ recipes/server-agent.rb
@@ -2,7 +2,7 @@ include_recipe 'newrelic::repository'
 
 package 'newrelic-sysmond'
 
-execute "nrsysmond-config --set license_key=#{newrelic['license']}"
+execute "nrsysmond-config --set license_key=#{node['newrelic']['license']}"
 
 service 'newrelic-sysmond' do
   action [:enable, :start]
