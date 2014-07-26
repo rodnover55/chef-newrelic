07e9af5ab0a3da63077313b4cdaf9da6bef48d99
Melnikov Sergey <me@rnr.name>
2014-07-26 17:20:30 +0700

    Changed install repo package.

--- recipes/repository.rb
+++ recipes/repository.rb
@@ -7,7 +7,13 @@ case node["platform"]
       notifies :run, 'execute[apt-get update]', :immediately
     end
   when 'redhat', 'centos', 'fedora'
-    package 'newrelic_repo' do
+    remote_file "#{Chef::Config[:file_cache_path]}/newrelic-repo-5-3.noarch.rpm" do
       source 'http://download.newrelic.com/pub/newrelic/el5/i386/newrelic-repo-5-3.noarch.rpm'
+      action :create
+    end
+
+    rpm_package "newrelic-repo" do
+      source "#{Chef::Config[:file_cache_path]}/newrelic-repo-5-3.noarch.rpm"
+      action :install
     end
 end
