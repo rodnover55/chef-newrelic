80ad743b8fd09d0ab5512432b011bab4637e0dca
Melnikov Sergey <me@rnr.name>
2014-07-26 17:07:38 +0700

    Added rpm repo.

--- recipes/repository.rb
+++ recipes/repository.rb
@@ -1,8 +1,13 @@
 
-if node['platform_family'] == 'debian'
-  include_recipe 'apt'
-  execute 'wget -O - http://download.newrelic.com/548C16BF.gpg | sudo apt-key add -'
-  execute 'sh -c \'echo "deb http://apt.newrelic.com/debian/ newrelic non-free" > /etc/apt/sources.list.d/newrelic.list\'' do
-    notifies :run, 'execute[apt-get update]', :immediately
-  end
+case node["platform"]
+  when "debian", "ubuntu"
+    include_recipe 'apt'
+    execute 'wget -O - http://download.newrelic.com/548C16BF.gpg | sudo apt-key add -'
+    execute 'sh -c \'echo "deb http://apt.newrelic.com/debian/ newrelic non-free" > /etc/apt/sources.list.d/newrelic.list\'' do
+      notifies :run, 'execute[apt-get update]', :immediately
+    end
+  when 'redhat', 'centos', 'fedora'
+    package 'newrelic_repo' do
+      source 'http://download.newrelic.com/pub/newrelic/el5/i386/newrelic-repo-5-3.noarch.rpm'
+    end
 end
